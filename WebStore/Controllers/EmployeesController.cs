1028cac4959ae442b97dbb93b9fa5e2567321846
Татьяна Цынкова <volkovat4@rambler.ru>
2020-12-04 19:06:00 +0300

    Выполнено: 1. Создать интерфейс и сервис для работы с сотрудниками 2. Необходимо обеспечить возможность создания новых сотрудников при помощи форм. 3. Необходимо обеспечить возможность редактирования, удаления и сохранения изменений для сотрудников в списке при помощи созданной формы. 4. *Необходимо выводить ошибки при создании/редактировании пользователя.

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,25 +1,124 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
+    //[Route("users")]
     public class EmployeesController : Controller
     {
-        private List<Employee> _Employees;
+        private readonly IEmployeesData _Employees;
 
-        public EmployeesController() => _Employees = TestData.Employees;
+        public EmployeesController(IEmployeesData Employees) => _Employees = Employees;
 
-        public IActionResult Index() => View(TestData.Employees);
+        public IActionResult Index()
+        {
+            var employees = _Employees.Get();
+            return View(employees);
+        }
 
+       // [Route("{id}")]
         public IActionResult Details(int id)
         {
-            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            var employee = _Employees.Get(id);
             if (employee is not null)
                 return View(employee);
-            else return NotFound();
+            return NotFound();
         }
+
+        //[Route("create")]
+        public IActionResult Create() => View("Edit", new EmployeesViewModel());
+
+        //[Route("edit")]
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+                return View(new EmployeesViewModel());
+
+            if (id < 0)
+                return BadRequest();
+
+            var employee = _Employees.Get((int)id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeesViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                Name = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                Age = employee.Age,
+                Phone = employee.Phone,
+                City = employee.City,
+                Position = employee.Position,
+                EmploymentDate = employee.EmploymentDate,
+            });
+        }
+
+        [HttpPost]
+       // [Route("edit")]
+        public IActionResult Edit(EmployeesViewModel Model)
+        {
+            if (Model is null)
+                throw new ArgumentNullException(nameof(Model));
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                LastName = Model.LastName,
+                FirstName = Model.Name,
+                MiddleName = Model.MiddleName,
+                Age = Model.Age,
+                Phone = Model.Phone,
+                City = Model.City,
+                Position = Model.Position,
+                EmploymentDate = Model.EmploymentDate,
+            };
+
+            if (employee.Id == 0)
+                _Employees.Add(employee);
+            else
+                _Employees.Update(employee);
+
+            return RedirectToAction("Index");
+        }
+
+        //[Route("delete")]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var employee = _Employees.Get(id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeesViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                Name = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                Age = employee.Age,
+                Phone = employee.Phone,                
+                City = employee.City,
+                Position = employee.Position,
+                EmploymentDate = employee.EmploymentDate,
+            });
+        }
+
+        [HttpPost]
+       // [Route("deleteConfirmed")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _Employees.Delete(id);
+            return RedirectToAction("Index");
+        }        
     }
 }
---
+++ WebStore/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        public int Id { get; set; }
+
+        /// <summary>Имя</summary>
+        [Required(ErrorMessage = "Не указано имя")]
+        [RegularExpression(@"([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$", ErrorMessage = "Некорректное имя")]
+        public string Name { get; set; }
+
+        /// <summary>Фамилия</summary>
+        [Required(ErrorMessage = "Не указана фамилия")]
+        [RegularExpression(@"([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$", ErrorMessage = "Некорректная фамилия")]
+        public string LastName { get; set; }
+
+        /// <summary>Отчество</summary>
+        [RegularExpression(@"([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$", ErrorMessage = "Некорректное отчество")]
+        public string MiddleName { get; set; }
+
+        /// <summary>Возраст</summary>
+        [Required(ErrorMessage = "Не указан возраст")]
+        [Range(18, 110, ErrorMessage = "Недопустимый возраст")]
+        public int Age { get; set; }
+
+        /// <summary>Телефон</summary>
+        [Required(ErrorMessage = "Не указан телефон")]
+        public string Phone { get; set; }
+
+        /// <summary>Город</summary>
+        [Required(ErrorMessage = "Не указан город")]
+        public string City { get; set; }
+
+        /// <summary>Должность</summary>
+        [Required(ErrorMessage = "Не указана должность")]
+        public string Position { get; set; }
+
+        /// <summary>Дата трудоустройства</summary>
+        [Required(ErrorMessage = "Не указана дата трудоустройства")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public DateTime EmploymentDate { get; set; }
+    }
+}
+
